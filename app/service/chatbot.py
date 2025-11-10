48c22fc54411adef110b1866f6fa7533486bee79
en-rypted <shivwackchaure19@gmail.com>
2025-11-10 17:58:31 +0530

    Add resume text extraction and update system prompt to include resume information for chatbot interactions.

--- app/service/chatbot.py
+++ app/service/chatbot.py
@@ -15,6 +15,13 @@ for page in reader.pages:
     if text:
         linkedin += text
 
+reader = PdfReader("app/resources/resume.pdf")
+resume = ""
+for page in reader.pages:
+    text = page.extract_text()
+    if text:
+        resume += text
+
 print(linkedin)
 
 with open("app/resources/summary.txt", "r", encoding="utf-8") as f:
@@ -25,12 +32,12 @@ name = "Shiv"
 system_prompt = f"You are acting as {name}. You are answering questions on {name}'s website, \
 particularly questions related to {name}'s career, background, skills and experience. \
 Your responsibility is to represent {name} for interactions on the website as faithfully as possible. \
-You are given a summary of {name}'s background and LinkedIn profile which you can use to answer questions. \
+You are given a summary of {name}'s background and LinkedIn profile and resumewhich you can use to answer questions. \
 Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
 If you don't know the answer, say so."
 
-system_prompt += f"\n\n## Summary:\n{summary}\n\n## LinkedIn Profile:\n{linkedin}\n\n"
-system_prompt += f"With this context, please chat with the user, always staying in character as {name}."
+system_prompt += f"\n\n## Summary:\n{summary}\n\n## LinkedIn Profile:\n{linkedin}\n\n## Resume:\n{resume}\n\n"
+system_prompt += f"With this context, please chat with the user, always staying in character as {name} and give the answers in short and concise manner and don't write in markdown format."
 
 def chat(message, history):
     messages = [{"role": "system", "content": system_prompt}] + history + [{"role": "user", "content": message}]
